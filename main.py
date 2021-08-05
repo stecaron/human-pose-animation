556cb953db22742f97178497326450182a1fdac4
Stephane Caron <ste.caron@icloud.com>
2021-08-04 21:23:13 -0400

    First version with a sample video

---
+++ main.py
@@ -0,0 +1,21 @@
+import cv2
+import mediapipe as mp
+
+mpDraw = mp.solutions.drawing_utils
+mpPose = mp.solutions.pose
+pose = mpPose.Pose()
+
+cap = cv2.VideoCapture('videos/video-1.mp4')
+
+while True:
+    success, img = cap.read()
+
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = pose.process(imgRGB)
+
+    if results.pose_landmarks:
+        mpDraw.draw_landmarks(img, results.pose_landmarks, mpPose.POSE_CONNECTIONS)
+
+    cv2.imshow('Image', img)
+
+    cv2.waitKey(1)
